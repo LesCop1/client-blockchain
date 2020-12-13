730e9c15570ae336b0740f5aa93f9c839953f0df
anthoback <backerantho@gmail.com>
2020-12-13 16:24:05 +0100

    page de connexions entière (sauf icone)

--- src/components/ExampleComponent.tsx
+++ src/components/ExampleComponent.tsx
@@ -1,26 +1,54 @@
-import Button from "@material-ui/core/Button/Button";
 import React from "react";
-import "./Button.scss";
+import "./index.scss";
 
 type StringAccount = {
-	type: "Email" | "Password";
+	type: "Email" | "Password" | "Nom" | "prenom";
+	categorie: "1" | "2" | "3";
 };
 
 export default function TextArea(props: StringAccount): JSX.Element {
+	// eslint-disable-next-line no-nested-ternary
 	return props.type === "Email" ? (
 		<div>
 			<label htmlFor="Type">
 				Email :
 				<br />
-				<input type="text" placeholder="patrickxx@example.com" required />
+				<input size={30} className="shift_input style" type="text" placeholder="Example@example.com" required />
+			</label>
+		</div>
+	) : // eslint-disable-next-line no-nested-ternary
+	props.type === "Password" ? (
+		props.categorie === "1" ? (
+			<div>
+				<label htmlFor="Type">
+					Mot de passe :
+					<br />
+					<input size={30} className="shift_input style" type="password" placeholder="•••••••••" required />
+				</label>
+			</div>
+		) : (
+			<div>
+				<label htmlFor="Type">
+					Confirmer le mot de passe :
+					<br />
+					<input size={30} className="shift_input style" type="password" placeholder="•••••••••" required />
+				</label>
+			</div>
+		)
+	) : props.type === "prenom" ? (
+		<div>
+			<label htmlFor="Type">
+				Prenom :
+				<br />
+				<input size={30} className="shift_input style" type="Text" placeholder="André" required />
 			</label>
 		</div>
 	) : (
 		<div>
 			<label htmlFor="Type">
-				Mot de passe :
+				Nom :
 				<br />
-				<input type="text" placeholder="**********" required />
+				<input size={30} className="shift_input style" type="Text" placeholder="Jhonson" required />
 			</label>
 		</div>
 	);
@@ -28,8 +56,17 @@ export default function TextArea(props: StringAccount): JSX.Element {
 
 export function ButtonValidation(): JSX.Element {
 	return (
-		<Button className="validation" variant="contained" href="#contained-buttons">
+		<button className="validation" type="submit">
 			Valider
-		</Button>
+		</button>
+	);
+}
+
+export function Icone(): JSX.Element {
+	return (
+		<div>
+			<div className="icone" />
+			<div>Ecocoin</div>
+		</div>
 	);
 }
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,18 +1,40 @@
 import React from "react";
-import TextArea, { ButtonValidation } from "../components/ExampleComponent";
+import { Link } from "react-router-dom";
+import TextArea, { ButtonValidation, Icone } from "../components/ExampleComponent";
 
 export default class Homepage extends React.Component<void, void> {
 	render(): JSX.Element {
 		return (
-			<div>
-				<div>
-					<h1>Se connecter </h1>
-					<br />
-					<TextArea type="Email" />
-					<br />
-					<TextArea type="Password" />
+			<div className="back">
+				<Icone />
+				<div className="center">
+					<div className="form">
+						<div>
+							<br />
+							<h1 className="center text_color_main">Se connecter </h1>
+
+							<div className="center text_color_main">
+								<TextArea type="Email" categorie="1" />
+							</div>
+
+							<div className="center text_color_main">
+								<TextArea type="Password" categorie="1" />
+							</div>
+
+							<Link className="link_fp" to="/Homepage">
+								mot de passe oublié ?
+							</Link>
+						</div>
+						<div className="center">
+							<ButtonValidation />
+						</div>
+						<Link className="center text_color_main" to="/Homepage">
+							<strong>S&apos;enregistrer</strong>
+						</Link>
+						<br />
+						<br />
+					</div>
 				</div>
-				<ButtonValidation />
 			</div>
 		);
 	}
