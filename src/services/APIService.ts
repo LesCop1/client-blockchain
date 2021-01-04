ad4c0e41c47fda25a5d25bbbfa8b1114c321c462
Mathis Engels <mathisengels@yahoo.fr>
2021-01-04 22:36:55 +0100

    Created the Dashboard Layout, and Rest Client

---
+++ src/services/APIService.ts
@@ -0,0 +1,48 @@
+import axios, { AxiosError, AxiosRequestConfig } from "axios";
+import { API_URL } from "../globals";
+
+type Response = {
+	code: number;
+};
+
+export type SuccessResponse = {
+	data: unknown;
+} & Response;
+
+export type ErrorResponse = {
+	error: {
+		message: string;
+		stack?: unknown;
+	};
+} & Response;
+
+async function request(config: AxiosRequestConfig): Promise<SuccessResponse | ErrorResponse> {
+	try {
+		const res = await axios({ ...config, withCredentials: true, responseType: "json" });
+
+		return {
+			code: res.status,
+			data: res.data,
+		};
+	} catch (error) {
+		return {
+			code: Number((<AxiosError>error).code) || 500,
+			error: {
+				message: (<AxiosError>error).message || "An error occured",
+				stack: (<AxiosError>error).stack,
+			},
+		};
+	}
+}
+
+async function get(path: string): Promise<SuccessResponse | ErrorResponse> {
+	const url = API_URL + path;
+	return request({ method: "GET", url });
+}
+
+async function post(path: string, body: unknown): Promise<SuccessResponse | ErrorResponse> {
+	const url = API_URL + path;
+	return request({ method: "POST", url, data: body });
+}
+
+export default { get, post };
