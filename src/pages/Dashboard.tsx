73ee9a9943c8f09e91a012f39bf9cb84f649c176
BuildTools <unconfigured@null.spigotmc.org>
2021-01-05 15:19:44 +0100

    Made panels Swipeable

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
+import SwipeableViews from "react-swipeable-views";
 import TabPanel from "../components/TabPanel";
 
 export default class Dashboard extends Component<void, { tabIndex: number }> {
@@ -19,26 +20,26 @@ export default class Dashboard extends Component<void, { tabIndex: number }> {
 			<div>
 				<AppBar position="static">
 					<Tabs className="tabsStyle" variant="scrollable" scrollButtons="auto" value={this.state.tabIndex} onChange={this.handleTabChange}>
-						<Tab className="tabsStyle" label="Tableau de bord" />
-						<Tab className="tabsStyle" label="Acheter / Vendre" />
-						<Tab className="tabsStyle" label="Miner" />
-						<Tab className="tabsStyle" label="Paramètres" />
-						<Tab className="tabsStyle" label="Aide" />
-						<Tab className="tabsStyle" label="Contact" />
+						<Tab className="tabStyle" label="Tableau de bord" />
+						<Tab className="tabStyle" label="Acheter / Vendre" />
+						<Tab className="tabStyle" label="Miner" />
+						<Tab className="tabStyle" label="Paramètres" />
+						<Tab className="tabStyle" label="Aide" />
+						<Tab className="tabStyle" label="Contact" />
 					</Tabs>
 				</AppBar>
 
-				{/* TODO SwipeableTab */}
-
-				<TabPanel value={this.state.tabIndex} index={0}>
-					Test
-				</TabPanel>
-				<TabPanel value={this.state.tabIndex} index={1}>
-					Page Two
-				</TabPanel>
-				<TabPanel value={this.state.tabIndex} index={2}>
-					Page Three
-				</TabPanel>
+				<SwipeableViews index={this.state.tabIndex}>
+					<TabPanel value={this.state.tabIndex} index={0}>
+						Test
+					</TabPanel>
+					<TabPanel value={this.state.tabIndex} index={1}>
+						Page Two
+					</TabPanel>
+					<TabPanel value={this.state.tabIndex} index={2}>
+						Page Three
+					</TabPanel>
+				</SwipeableViews>
 			</div>
 		);
 	}
